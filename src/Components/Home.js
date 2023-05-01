3dfd7cdbed2b889037db1f82656fed6a81fb5be7
Mahir <mahirmanandhar02@gmail.com>
2023-05-01 09:36:00 +0545

    first commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="bg img-fluid ">
         <div className="align">
           <br />
-          <br />
+          <br/>
           WELCOME TO
           <br />
           THE
