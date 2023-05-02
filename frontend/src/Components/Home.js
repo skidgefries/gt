22ccd297d7988b1568bf1f6ae6ace9ac87b9aa86
Anusha <anusha@gmail.com>
2023-05-02 18:03:37 +0545

    asdfghjk

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <div className="center">
-        <div div className="bg img-fluid ">
+        <div  className="bg img-fluid ">
           <div className="align">
             <br />
             <br />
