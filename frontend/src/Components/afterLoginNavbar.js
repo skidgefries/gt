2690124ca02641728125fa6f3658e7da85929a44
0-PRK <pranawrajkafle.prk@gmail.com>
2023-05-23 22:18:34 +0545

    Error chaldaina

--- frontend/src/Components/afterLoginNavbar.js
+++ frontend/src/Components/afterLoginNavbar.js
@@ -10,45 +10,52 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} `}
     >
       <div className="container-fluid">
-
-          <NavLink className="navbar-brand" to="/LoginHome">
-            <img
-              src={logo}
-              alt="Logo"
-              width="150"
-              height="70"
-              className=" align-text-top"
-            />
-          </NavLink>
-          <div className="rightNav">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+        <NavLink className="navbar-brand" to="/LoginHome">
+          <img
+            src={logo}
+            alt="Logo"
+            width="150"
+            height="70"
+            className=" align-text-top"
+          />
+        </NavLink>
+        <div className="rightNav">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className=" navbar-nav me-auto mb-2 mb-lg-0  ">
-            
-            <li className="nav-item"><a
-            className="nav-link"
-            style={{ font: "Montserrat", fontSize: 30 }}
-            href="/"
-          >
-            <input
-              className="form-check-input"
-              style={{ font: "Montserrat", fontSize: 33 }}
-              onClick={props.toggleMode}
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckReverse"
-            />
-            <label
-              className="form-check-label"
-              style={{ font: "Montserrat", fontSize: 30 }}
-              htmlFor="flexSwitchCheckReverse"
-            >
-              ENABLE DARKMODE
-            </label>
-          </a>
-          </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  style={{ font: "Montserrat", fontSize: 30 }}
+                  href="/"
+                >
+                  <input
+                    className="form-check-input"
+                    style={{ font: "Montserrat", fontSize: 33 }}
+                    onClick={props.toggleMode}
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckReverse"
+                  />
+                  <label
+                    className="form-check-label"
+                    style={{ font: "Montserrat", fontSize: 30 }}
+                    htmlFor="flexSwitchCheckReverse"
+                  >
+                    ENABLE DARKMODE
+                  </label>
+                </a>
+              </li>
               <li className="nav-item">
                 <NavLink
                   className="nav-link "
@@ -58,7 +65,7 @@ export default function Navbar(props) {
                   HOME
                 </NavLink>
               </li>
-             
+
               <li className="nav-item">
                 <NavLink
                   className="nav-link "
@@ -66,9 +73,9 @@ export default function Navbar(props) {
                   to="./About"
                 >
                   ABOUT US
-                </NavLink> 
+                </NavLink>
               </li>
-        
+
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
@@ -95,13 +102,17 @@ export default function Navbar(props) {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                  <NavLink to="/Home" onClick={()=>props.onFormSwitch("home")} className="dropdown-item" href="/">
+                    <NavLink
+                      to="/Home"
+                      onClick={() => props.onFormSwitch("home")}
+                      className="dropdown-item"
+                      href="/"
+                    >
                       Log Out
                     </NavLink>
                   </li>
                 </ul>
               </li>
-             
 
               {/* <li className="nav-item">
                 <a className="nav-link disabled" href="/" tabIndex="-1" aria-disabled="true">Disabled</a>
@@ -112,14 +123,12 @@ export default function Navbar(props) {
                 className={`form-check form-switch form-check-reverse text-${
                   props.mode === "light" ? "dark" : "light"
                 }`}
-              >
-
-              </div>
+              ></div>
             </ul>
           </div>
         </div>
-        </div>
-        {/*<div className="right">
+      </div>
+      {/*<div className="right">
           <form className="d-flex" role="search">
             <input
               className="form-control me-2"
